0cee821b86a9fa45780d959aeed1ab646726486c
wwwbrunagon <wwwbrunagon@gmail.com>
2023-04-12 17:45:35 -0300

    birthday-cake-candle spec

--- javascript/hackerhank/easy/birthday-cake-candles/birthday-cake-candles.spec.js
+++ javascript/hackerhank/easy/birthday-cake-candles/birthday-cake-candles.spec.js
@@ -6,4 +6,16 @@ describe('birthdayCakeCandles(candles)', () => {
             expect(birthdayCakeCandles([3])).toEqual(1);
         });
     });
+
+    describe('array with no repeated numbers', () => {
+        test('should find 5 one time', () => {
+            expect(birthdayCakeCandles([5, 3])).toEqual(1);
+        });
+    });
+
+    describe('array with repeated numbers', () => {
+        test('should find 7 three times', () => {
+            expect(birthdayCakeCandles([7, 3, 5, 1, 7, 7, 2])).toEqual(3);
+        });
+    });
 });
