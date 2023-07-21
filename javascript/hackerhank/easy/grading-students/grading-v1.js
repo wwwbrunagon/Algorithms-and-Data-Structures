2ed15441be1262072fb90dc39936f95491ab2a69
wwwbrunagon <wwwbrunagon@gmail.com>
2023-07-21 14:45:19 -0300

    grading hackerrank v1 wip

--- javascript/hackerhank/easy/grading-students/grading-v1.js
+++ javascript/hackerhank/easy/grading-students/grading-v1.js
@@ -1,14 +1,24 @@
 function round(grade) {
     return grade;
-};
-
-
-function gradeStudents(grades) {
+  }
+  
+  // [37, 52, 80, 100, 24]
+  
+  function gradeStudents(grades) {
     const result = [];
+  
     for (let i = 0; i < grades.length; ++i) {
-        const grade = grades[i];        
+      const grade = grades[i];
+  
+      if (grade < 40) {
+        result.push(grade);
+      } else {
+        result.push(round(grade));
+      }
     }
-    return grades
-};
-
-export { gradeStudents, round }
+  
+    return result;
+  }
+  
+  export { round, gradeStudents };
+  
--- javascript/hackerhank/easy/grading-students/grading-v1.spec.js
+++ javascript/hackerhank/easy/grading-students/grading-v1.spec.js
@@ -1,41 +1,44 @@
-import { gradeStudents, round } from "./grading-v1";
 
-describe('round()',  () => {
-    describe('whenthe difference is NOT less then 3', () => {
-        it('should not rounf up', () => {
-            expect(round(90)).toEqual(90);
-    
-            // 75 - 71 = 4
-            expect(round(71)).toEqual(71);
-    
-            // 100 - 96 = 4
-            expect(round(96)).toEqual(96);
-    
-            // 100 - 97 = 3
-            expect(round(97)).toEqual(96);
-        });
+describe('round()', () => {
+    describe('when the difference is NOT less than 3', () => {
+      it('should not round up', () => {
+        // 95 - 90 = 5
+        expect(round(90)).toEqual(90);
+  
+        // 75 - 71 = 4
+        expect(round(71)).toEqual(71);
+  
+        // 45 - 42 = 3
+        expect(round(42)).toEqual(42);
+  
+        // 100 - 96 = 4
+        expect(round(96)).toEqual(96);
+  
+        // 100 - 97 = 3
+        expect(round(97)).toEqual(97);
+      });
     });
-
-
-    describe('whenthe difference IS less then 3', () => {
-        it('should rounf up', () => {
-            // 45 - 42 = 2
-            // 3 - 1 < 3
-            expect(round(43)).toEqual(45);    
-        });
+  
+    describe.only('when the difference IS less than 3', () => {
+      it('should round up', () => {
+        // 45 - 43 = 2, then round up!
+        expect(round(43)).toEqual(45);
+      });
     });
-});
-
-describe('gradeStudents', () => {
+  });
+  
+  describe.skip('Grading Students', () => {
     describe('when the grade is < 38', () => {
-        it('should return the number without any rounding', () => {
-            expect(gradeStudents([37])).toEqual([37]);
-        });
-    }); 
-    
+      it('should return the number without any rounding', () => {
+        expect(gradeStudents([37])).toEqual([37]);
+        expect(gradeStudents([13])).toEqual([13]);
+      });
+    });
+  
     describe('when the grade is >= 38', () => {
-        it('should return 42 and not round to 45', () => {
-            expect(gradeStudents([42])).toEqual([42])
-        })
-    })
-})
+      it('should return 42 and not round to 45', () => {
+        expect(gradeStudents([42])).toEqual([42]);
+      });
+    });
+  });
+  
