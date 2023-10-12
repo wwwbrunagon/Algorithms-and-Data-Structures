34bcbeb3ce16325ff59238a011bf0ef5bbb4d5e7
wwwbrunagon <wwwbrunagon@gmail.com>
2023-10-12 09:12:13 -0300

    hackerhank problem/apple-and-orange (WIP)

---
+++ javascript/hackerhank/easy/apple-and-orange/problem/apple-and-orange.js
@@ -0,0 +1,9 @@
+/**
+ * Count how many apples and oranges fall within a given house.
+ */
+
+function count(houseStart, houseEnd, appleTree, orangeTree, apples, oranges) {
+  console.log({ houseStart, houseEnd, appleTree, orangeTree, apples, oranges });
+}
+
+count(7, 10, 4, 12, [4, 2, 6, -3], []);
